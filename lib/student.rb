212dde4710a998c94f64cc03ba43f180dde222b6
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-10 20:16:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,7 @@
 class Student
   attr_accessor :id, :name, :grade
 
-def initialize(id:, name:, grade:)
-  @id = id
-  @name = name
-  @grade = grade
-end
+
 
   def self.new_from_db(row)
     new_student = Student.new
