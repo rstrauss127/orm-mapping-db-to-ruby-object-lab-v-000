094c22774e387ce27b8e4ac514b07bb0c59e8822
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-10 20:40:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
         SELECT * FROM students WHERE grade = 9
         SQL
 
-        DB[:conn].execute(sql, grade).map do |row|
+        DB[:conn].execute(sql).map do |row|
           self.new_from_db(row)
         end
       end
