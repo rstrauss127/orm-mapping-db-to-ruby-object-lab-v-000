e0937bc450d292c30c27bb4a1f4a323f2f9f7292
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-10 21:02:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
     end
 
     def self.first_X_students_in_grade_10(x)
-  
+
       sql = <<-SQL
         SELECT * FROM students
         WHERE grade = 10
@@ -62,7 +62,7 @@ class Student
       end
 
       def self.first_student_in_grade_10
-        self.first_X_students_in_grade_10(1)
+      Student.new_from_db(self.first_X_students_in_grade_10(1))
 
 
       end
