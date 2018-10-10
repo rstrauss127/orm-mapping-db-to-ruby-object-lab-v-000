8b332da7ef6fd799ba320b9ba7c687d227388d6a
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-10 21:03:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
       end
 
       def self.first_student_in_grade_10
-      Student.new_from_db(self.first_X_students_in_grade_10(1))
+      self.first_X_students_in_grade_10(1)[0]
 
 
       end
